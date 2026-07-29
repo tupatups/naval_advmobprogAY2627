56b362007b605c8d934492461b243cc496d6b96c
Jozza Nerel Tucio <jozzatucio@Jozzas-MacBook-Air.local>
2026-07-30 03:00:21 +0800

    lab_act1

---
+++ naval_mobile/lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.onOpenThemePage});
+
+  final VoidCallback onOpenThemePage;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePagePageState();
+}
+
+class _MyHomePagePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ephemeral State Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: widget.onOpenThemePage,
+              child: const Text('Go to Change Theme Page'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
